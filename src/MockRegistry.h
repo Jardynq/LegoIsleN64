bf00960d37858761e60d9d7f4b8000948feb4736
Christian Jordan <gcp547@alumni.ku.dk>
2025-04-02 19:44:26 +0200

    Cleanup unnecessary files

--- src/MockRegistry.h
+++
@@ -1,9 +0,0 @@
-#ifndef MOCK_REG_H
-#define MOCK_REG_H
-
-enum RegType { RegString, RegInt, RegBool };
-void RegReset();
-int RegReadKey(const char* key, enum RegType type, void* output, int max_len);
-int RegWriteKey(const char* key, enum RegType type, const void* input);
-
-#endif // MOCK_REG_H
