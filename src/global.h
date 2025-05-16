5a11d405b84d31cd98575940cd92145d571c9995
Christian Jordan <gcp547@alumni.ku.dk>
2025-05-16 11:29:17 +0200

    Update global.h

--- src/global.h
+++ src/global.h
@@ -1,11 +1,11 @@
 #pragma once
+#include "global/types.h"
 
+#include "global/cursor.h"
 #include "global/debug.h"
 #include "global/math.h"
 #include "global/string.h"
-#include "global/types.h"
 
 #include <libdragon.h>
 #include <t3d/t3d.h>
 #include <t3d/t3dmodel.h>
-
